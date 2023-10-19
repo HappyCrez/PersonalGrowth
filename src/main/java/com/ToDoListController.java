e16a279b62cad06d75123bdd924fe0ac33921654
HappyCrez <gorbunov-2004@bk.ru>
2023-10-19 19:39:47 +0700

    refactoring ToDoList(controller/view)

--- src/main/java/com/ToDoListController.java
+++ src/main/java/com/ToDoListController.java
@@ -21,64 +21,52 @@ public class ToDoListController {
     private DatePicker DatePicker;
 
     @FXML
-    private Button addButton;
+    private Label statusLabel;
 
     @FXML
-    private Label addLabel;
-
-    @FXML
-    private Button completedButton;
+    private Button completedButton, addButton, todayButton, tomorrowButton, weekButton;
 
     @FXML
     private ListView<?> taskList;
 
     @FXML
-    private Button todayButton;
-
-    @FXML
-    private Button tomorrowButton;
-
-    @FXML
-    private Button weekButton;
-
-    @FXML
     void addButtonClicked(ActionEvent event) {
-        addLabel.setText("Задача добавлена!");
+        statusLabel.setText("Задача добавлена!");
     }
 
     @FXML
     void completedButtonClicked(ActionEvent event) {
-        addLabel.setText("Выполненые задачи");
+        statusLabel.setText("Выполненые задачи");
     }
 
     @FXML
     void todayButtonClicked(ActionEvent event) {
-        addLabel.setText("Задачи на сегодня");
+        statusLabel.setText("Задачи на сегодня");
     }
 
     @FXML
     void tomorrowButtonClicked(ActionEvent event) {
-        addLabel.setText("Задачи на завтра!");
+        statusLabel.setText("Задачи на завтра!");
     }
 
     @FXML
     void weekButtonClicked(ActionEvent event) {
-        addLabel.setText("Задачи на неделю");
+        statusLabel.setText("Задачи на неделю");
     }
 
     @FXML
     void toHabbitsClicked(ActionEvent event) {
-        addLabel.setText("Вы перешли к трекеру привычек");
+        statusLabel.setText("Вы перешли к трекеру привычек");
     }
 
     @FXML
     void toMenuClicked(ActionEvent event) {
-        addLabel.setText("Вы перешли в меню");
+        statusLabel.setText("Вы перешли в меню");
     }
 
     @FXML
     void toTimerClicked(ActionEvent event) {
-        addLabel.setText("Вы перешли к таймеру фокусировки");
+        statusLabel.setText("Вы перешли к таймеру фокусировки");
     }
 
     @FXML
