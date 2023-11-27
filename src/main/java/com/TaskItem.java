834ec13d5e9567839205a82fcfdc67df12dddc87
Купалов Павел <kupalovpaul@gmail.com>
2023-11-27 17:03:49 +0700

    Added day selection functions

--- src/main/java/com/TaskItem.java
+++ src/main/java/com/TaskItem.java
@@ -1,40 +1,54 @@
 package com;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.CheckBox;
+import java.time.LocalDate;
 
-public class TaskList {
+public class TaskItem {
 	private SimpleStringProperty titleTask = new SimpleStringProperty();
 	private SimpleStringProperty descriptionTask = new SimpleStringProperty();
 	private SimpleStringProperty dateTask = new SimpleStringProperty();
-	
-	public TaskList(String titleTask, String descriptionTask, String dateTask) {
-		super();
+	private LocalDate date;
+	private CheckBox checkBox;
+	private static int count = 0;
+
+	public TaskItem(String titleTask, String descriptionTask, LocalDate dateTask) {
 		this.titleTask = new SimpleStringProperty(titleTask);
 		this.descriptionTask = new SimpleStringProperty(descriptionTask);
-		this.dateTask = new SimpleStringProperty(dateTask);
+		this.date = dateTask;
+		this.dateTask = new SimpleStringProperty(date.toString());
+		this.checkBox = new CheckBox();
+		this.checkBox.setId(Integer.toString(++count));
 	}
-	
 	public String getTitleTask() {
 		return titleTask.get();
 	}
-	
-	public void setTitleTask(String titleTask) {
-		this.titleTask.set(titleTask);
-	}
-	
+
 	public String getDescriptionTask() {
 		return descriptionTask.get();
+	}
+
+	public LocalDate getDateTask() {
+		return date;
+	}
+
+	public CheckBox getCheckBox() {
+		return checkBox;
+	}
+
+	public void setTitleTask(String titleTask) {
+		this.titleTask.set(titleTask);
 	}
 	
 	public void setDescriptionTask(String descriptionTask) {
 		this.descriptionTask.set(descriptionTask);
 	}
 	
-	public String getDateTask() {
-		return dateTask.get();
-	}
-	
 	public void setDateTask(String DateTask) {
 		this.dateTask.set(DateTask);
+	}
+
+	public void setCheckBox(CheckBox checkBox) {
+		this.checkBox = checkBox;
 	}
 }
--- src/main/java/com/ToDoListController.java
+++ src/main/java/com/ToDoListController.java
@@ -1,15 +1,13 @@
 package com;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class ToDoListController {
 
@@ -20,38 +18,36 @@ public class ToDoListController {
     private URL location;
 
     @FXML
-    private DatePicker DatePicker;
+    private DatePicker dateField;
 
     @FXML
     private Label statusLabel;
 
     @FXML
     private Button completedButton, addButton, todayButton, tomorrowButton, weekButton;
-    
+
     private ScreenController screenController;
-    @FXML
-    private TableView<TaskList> taskTableView;
 
     @FXML
-    private TableColumn<TaskList, String> titleTableColumn;
+    private TableView<TaskItem> taskTableView;
+
+    @FXML
+    private TableColumn<TaskItem, String> titleTableColumn, descriptionTableColumn, dateTableColumn;
+
+    @FXML
+    private TableColumn<TaskItem, CheckBox> checkBoxColumn;
 
     public ToDoListController(ScreenController screenController) {
-    	this.screenController = screenController;
+        this.screenController = screenController;
     }
-
-    @FXML
-    private TableColumn<TaskList, String> descriptionTableColumn;
-
-    @FXML
-    private TableColumn<TaskList, String> dateTableColumn;
     
     @FXML
-    private TextField titleTextField, descriptionTextField, dateTextField;
+    private TextField titleTextField, descriptionTextField;
 
     @FXML
     void addButtonClicked(ActionEvent event) {
-        TaskList taskList = new TaskList(titleTextField.getText(), descriptionTextField.getText(), dateTextField.getText());
-        taskTableView.getItems().add(taskList);
+        TaskItem taskItem = new TaskItem(titleTextField.getText(), descriptionTextField.getText(), dateField.getValue());
+        taskTableView.getItems().add(taskItem);
     }
 
     @FXML
@@ -61,17 +57,44 @@ public class ToDoListController {
 
     @FXML
     void todayButtonClicked(ActionEvent event) {
-        statusLabel.setText("Задачи на сегодня");
+        LocalDate nowDate = LocalDate.now();
+        ObservableList<TaskItem> list = taskTableView.getItems();
+        ObservableList<TaskItem> todayList = FXCollections.observableArrayList();
+        for (TaskItem e :  list) {
+            if (e.getDateTask().toString().equals(nowDate.toString())) {
+                todayList.add(e);
+            }
+        }
+        taskTableView.getItems().clear();
+        taskTableView.getItems().addAll(todayList);
     }
 
     @FXML
     void tomorrowButtonClicked(ActionEvent event) {
-        statusLabel.setText("Задачи на завтра!");
+        LocalDate nowDate = LocalDate.now().plusDays(1);
+        ObservableList<TaskItem> list = taskTableView.getItems();
+        ObservableList<TaskItem> todayList = FXCollections.observableArrayList();
+        for (TaskItem e :  list) {
+            if (e.getDateTask().toString().equals(nowDate.toString())) {
+                todayList.add(e);
+            }
+        }
+        taskTableView.getItems().clear();
+        taskTableView.getItems().addAll(todayList);
     }
 
     @FXML
     void weekButtonClicked(ActionEvent event) {
-        statusLabel.setText("Задачи на неделю");
+        LocalDate nowDate = LocalDate.now().plusDays(7);
+        ObservableList<TaskItem> list = taskTableView.getItems();
+        ObservableList<TaskItem> todayList = FXCollections.observableArrayList();
+        for (TaskItem e :  list) {
+            if (e.getDateTask().isBefore(nowDate)) {
+                todayList.add(e);
+            }
+        }
+        taskTableView.getItems().clear();
+        taskTableView.getItems().addAll(todayList);
     }
 
     @FXML
