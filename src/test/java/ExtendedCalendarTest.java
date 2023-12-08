641dac0c529632db47e77bf94deef16934db6576
HappyCrez <gorbunov-2004@bk.ru>
2023-12-08 08:26:09 +0700

    add testing directory | add mvn phase lifecicle - test

---
+++ src/test/java/ExtendedCalendarTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class ExtendedCalendarTest {
+
+    @Test
+    public void myTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
