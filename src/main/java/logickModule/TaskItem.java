8fb796429f6c3cac1df669c3bfb6c9795cd4765a
HappyCrez <gorbunov-2004@bk.ru>
2023-12-08 20:02:53 +0700

    Update view #2

--- src/main/java/logickModule/TaskItem.java
+++ src/main/java/logickModule/TaskItem.java
@@ -5,42 +5,50 @@ import java.time.format.DateTimeFormatter;
 
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
 public class TaskItem extends AnchorPane{
 	private Label contentField;
-	private CheckBox checkerField;
+	private RadioButton checkerField;
 	private Label dateField;
 
 	private LocalDate dateInfo;
 
 	public TaskItem(String content, LocalDate date) {
 		this.contentField = new Label(content);
+		this.contentField.getStyleClass().add("taskContent");
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyy");
 		this.dateField = new Label(date.format(format));
+		this.dateField.getStyleClass().add("secondaryInfo");
 		this.dateInfo = date;
-		this.checkerField = new CheckBox();
-		HBox group = new HBox(this.dateField, this.checkerField);
-		group.getStyleClass().add("task-left-group");
+		this.checkerField = new RadioButton();
+
 		
-		this.getChildren().addAll(this.contentField, group);
+		this.getChildren().addAll(this.contentField, this.checkerField, this.dateField);
 		this.getStyleClass().add("taskItem");
 
 
-		setTopAnchor(contentField, 10.0);
+		setTopAnchor(contentField, 0.0);
 		setBottomAnchor(contentField, 10.0);
-		setLeftAnchor(contentField, 0.0);
-		setTopAnchor(group, 10.0);
-		setBottomAnchor(group, 10.0);
-		setRightAnchor(group, 0.0);
+		setLeftAnchor(contentField, 20.0);
+		setRightAnchor(contentField, 0.0);
+
+		setTopAnchor(checkerField, 0.0);
+		setBottomAnchor(checkerField, 10.0);
+		setLeftAnchor(checkerField, -2.5);
+		
+		setBottomAnchor(dateField, -5.0);
+		setRightAnchor(dateField, 0.0);
+		setLeftAnchor(dateField, 0.0);
 	}
 	
 	public LocalDate getDateField() {
 		return dateInfo;
 	}
 
-	public CheckBox getCheckerField() {
+	public RadioButton getCheckerField() {
 		return checkerField;
 	}
 
@@ -52,7 +60,7 @@ public class TaskItem extends AnchorPane{
 		this.dateField.setText(DateTask);
 	}
 
-	public void setCheckerField(CheckBox checkBox) {
+	public void setCheckerField(RadioButton checkBox) {
 		this.checkerField = checkBox;
 	}
 }
