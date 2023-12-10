e94b487689b08ee494466b3b632d43c4a34cd017
HappyCrez <gorbunov-2004@bk.ru>
2023-12-10 19:34:25 +0700

    add delete btn to TaskItem | remove focused styles from textArea

--- src/main/java/logickModule/TaskItem.java
+++ src/main/java/logickModule/TaskItem.java
@@ -2,13 +2,19 @@ package logickModule;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
+import org.kordamp.ikonli.javafx.FontIcon;
+
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.layout.AnchorPane;
 
 public class TaskItem extends AnchorPane{
-	private Label contentField;
 	private RadioButton checkerField;
+	private Button deleteField;
+	
+	private Label contentField;
 	private Label dateField;
 	private Label groupField;
 
@@ -26,25 +32,31 @@ public class TaskItem extends AnchorPane{
 		groupField.getStyleClass().add("secondaryInfo");
 		
 		dateInfo = date;
+		deleteField = new Button("", new FontIcon("mdi-delete-forever"));
+		deleteField.getStyleClass().add("deleteBtn");
 		checkerField = new RadioButton();
 
-		this.getChildren().addAll(contentField, checkerField, dateField, groupField);
+		this.getChildren().addAll(contentField, checkerField, dateField, groupField, deleteField);
 		this.getStyleClass().add("taskItem");
 
 		setTopAnchor(contentField, 0.0);
 		setBottomAnchor(contentField, 10.0);
 		setLeftAnchor(contentField, 20.0);
-		setRightAnchor(contentField, 0.0);
+		setRightAnchor(contentField, 30.0);
+
+		setTopAnchor(deleteField, 0.0);
+		setBottomAnchor(deleteField, 0.0);
+		setRightAnchor(deleteField, -10.0);
 
 		setTopAnchor(checkerField, 0.0);
-		setBottomAnchor(checkerField, 10.0);
+		setBottomAnchor(checkerField, 0.0);
 		setLeftAnchor(checkerField, -2.5);
 		
 		setBottomAnchor(groupField, -5.0);
 		setLeftAnchor(groupField, 20.0);
 
 		setBottomAnchor(dateField, -5.0);
-		setRightAnchor(dateField, 0.0);
+		setRightAnchor(dateField, 30.0);
 	}
 	
 	public LocalDate getDateField() {
